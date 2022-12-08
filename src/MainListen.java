ff7ded6b8d213703da3c70eeaacddc83d5f2134a
Mag466q <77707803+Mag466q@users.noreply.github.com>
2022-12-08 14:26:40 +0100

    Update MainListen.java

--- src/MainListen.java
+++ src/MainListen.java
@@ -1,138 +1,134 @@
-import java.util.LinkedList;
 import java.util.Objects;
-
+import java.util.Stack;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
 
 public class MainListen extends CalculatorBaseListener {
 
-    private final LinkedList<Double> firstStack = new LinkedList<>();
-    private final LinkedList<Double> secondStack = new LinkedList<>();
+    private final Stack<Double> stack = new Stack<>();
 
     public Double getResult() {
-        return secondStack.pop();
+        return stack.peek();
     }
 
     private boolean symbolEquals(ParseTree child, int symbol) {
         return ((TerminalNode) child).getSymbol().getType() == symbol;
     }
-
     @Override
-    public void exitExpression(CalculatorParser.ExpressionContext ctx) {
-        Double result = secondStack.removeLast();
+    public void exitExpression(CalculatorParser.ExpressionContext ctx){
+        Double result = stack.pop();
+        System.out.println("# " + ctx.getChildCount());
         for (int i = 1; i < ctx.getChildCount(); i = i + 2) {
             if (symbolEquals(ctx.getChild(i), CalculatorParser.PLUS)) {
-                result += secondStack.removeLast();
+                result += stack.pop();
             } else {
-                result = result - secondStack.removeLast();
+                result =  stack.pop()- result;
             }
         }
-        secondStack.push(result);
-        System.out.println("Expression: \"" + ctx.getText() + "\" -> " + result);
+        stack.push(result);
+        System.out.println("Expression: \"" + ctx.getText() + "\" -> "+result);
     }
-
     @Override
-    public void exitMultiplyingExpression(CalculatorParser.MultiplyingExpressionContext ctx) {
-        Double result = firstStack.removeLast();
+    public void exitMultiplyingExpression(CalculatorParser.MultiplyingExpressionContext ctx){
+        Double result = stack.pop();
+        System.out.println("## " + ctx.getChildCount());
         for (int i = ctx.getChildCount() - 2; i >= 1; i = i - 2) {
             if (symbolEquals(ctx.getChild(i), CalculatorParser.TIMES)) {
-                result = firstStack.removeLast() * result;
-            } else if (symbolEquals(ctx.getChild(i), CalculatorParser.DIV)) {
-                result = firstStack.removeLast() / result;
-            } else {
-                result = firstStack.removeLast() % result;
+                result = stack.pop() * result;
+            }else if(symbolEquals(ctx.getChild(i), CalculatorParser.DIV)) {
+                result = stack.pop() / result;
+            }else {
+                result = stack.pop() % result ;
             }
         }
-        secondStack.push(result);
-        System.out.println("Expression: \"" + ctx.getText() + "\" -> " + result);
+        stack.push(result);
+        System.out.println("Expression: \"" + ctx.getText() + "\" -> "+result);
 
     }
 
     @Override
-    public void exitPovsqrExpression(CalculatorParser.PovsqrExpressionContext ctx) {
-
-        double result = firstStack.removeLast();
-        if (ctx.SQRT() == null) {
+    public void exitPovsqrExpression(CalculatorParser.PovsqrExpressionContext ctx){;
+        double result = stack.pop();
+        System.out.println("### " + ctx.getChildCount());
+        if(ctx.SQRT()==null) {
             for (int i = ctx.getChildCount() - 2; i >= 1; i = i - 2) {
                 if (symbolEquals(ctx.getChild(i), CalculatorParser.POV)) {
-                    result = Math.pow(firstStack.removeLast(), result);
+                    result = Math.pow(stack.pop(), result);
                 }
             }
-        } else {
+        }else{
             if (symbolEquals(ctx.getChild(0), CalculatorParser.SQRT)) {
                 result = Math.sqrt(result);
             }
         }
-        firstStack.push(result);
-        System.out.println("Expression: \"" + ctx.getText() + "\" -> " + result);
+        stack.push( result);
+        System.out.println("Expression: \"" + ctx.getText() + "\" -> "+result);
     }
 
 
     @Override
-    public void exitSing_simple(CalculatorParser.Sing_simpleContext ctx) {
+    public void exitSing_simple(CalculatorParser.Sing_simpleContext ctx){
+        double result = stack.pop();
 
-        double result = firstStack.removeLast();
-
-        if (ctx.MINUS() != null) {
+        if(ctx.LABS()!=null && ctx.PABS()!=null){
+            result = Math.abs(result);
+        }
+        if(ctx.MINUS()!=null){
             result = result * -1;
         }
 
-        if (ctx.funcname() != null) {
+
+
+        if(ctx.funcname()!=null) {
             for (int i = ctx.getChildCount(); i >= 1; i = i - 2) {
                 if (Objects.equals(ctx.funcname().getChild(i - 2).getText(), ctx.getChild(i - 2).getText())) {
-                    if (ctx.funcname().COS() != null) result = Math.cos(result);
-                    if (ctx.funcname().SIN() != null) result = Math.sin(result);
-                    if (ctx.funcname().TAN() != null) result = Math.tan(result);
-                    if (ctx.funcname().ACOS() != null) result = Math.acos(result);
-                    if (ctx.funcname().ASIN() != null) result = Math.asin(result);
-                    if (ctx.funcname().ATAN() != null) result = Math.atan(result);
-                    if (ctx.funcname().LOG() != null) result = Math.log(result);
-                    if (ctx.funcname().PI() != null) result = result * Math.PI;
-                    if (ctx.funcname().E() != null) result = result * Math.E;
+                    if(ctx.funcname().COS()!=null) result=Math.cos(result);
+                    if(ctx.funcname().SIN()!=null) result=Math.sin(result);
+                    if(ctx.funcname().TAN()!=null) result=Math.tan(result);
+                    if(ctx.funcname().ACOS()!=null) result=Math.acos(result);
+                    if(ctx.funcname().ASIN()!=null) result=Math.asin(result);
+                    if(ctx.funcname().ATAN()!=null) result=Math.atan(result);
+                    if(ctx.funcname().LOG ()!=null) result=Math.log(result);
+                    if(ctx.funcname().PI ()!=null) result=result * Math.PI;
+                    if(ctx.funcname().E ()!=null) result=result * Math.E;
                 }
             }
         }
-        System.out.println("Expression: \"" + ctx.getText() + "\" -> " + result);
-        firstStack.push(result);
-    }
+        System.out.println("Expression: \"" + ctx.getText() + "\" -> "+result);
+        stack.push(result);
 
+    }
 
     @Override
     public void exitSimple(CalculatorParser.SimpleContext ctx) {
             if (ctx.FLOAT() != null) {
                 double value = Double.parseDouble(ctx.FLOAT().getText());
-               firstStack.push(value);
+                stack.push(value);
                 System.out.println("Float:" + value);
             } else {
                 double value = Integer.parseInt(ctx.INT().getText());
-                firstStack.push(value);
+                stack.push(value);
                 System.out.println("INT:" + value);
             }
         }
 
-    public static Double calc(CharStream charStream) {
-        CalculatorLexer lexer = new CalculatorLexer(charStream);
+
+
+
+    public static void main(String[] args) throws Exception {
+        CharStream charStreams = CharStreams.fromFileName("C:\\Users\\mag46\\Documents\\Antlar44-master\\Antlar44-master\\src\\example.txt");
+        CalculatorLexer lexer = new CalculatorLexer(charStreams);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         System.out.println(tokens.getText());
 
         CalculatorParser parser = new CalculatorParser(tokens);
-        ParseTree tree = parser.expression();
+        ParseTree tree = parser.expression(); // parse
 
         ParseTreeWalker walker = new ParseTreeWalker();
-        MainListen calculatorListener = new MainListen();
-        walker.walk(calculatorListener, tree);
-        return calculatorListener.getResult();
-    }
-
-    public static Double calc(String expression) {
-        return calc(CharStreams.fromString(expression));
-    }
-
-    public static void main(String[] args) throws Exception {
-        CharStream charStreams = CharStreams.fromFileName("C:\\Users\\mag46\\Documents\\Antlar44-master\\Antlar44-master\\src\\example.txt");
-        Double result = calc(charStreams);
-        System.out.println("Result = " + result);
+        MainListen listener = new MainListen();
+        walker.walk(listener, tree);
+        System.out.println(listener.getResult());
     }
 
 
